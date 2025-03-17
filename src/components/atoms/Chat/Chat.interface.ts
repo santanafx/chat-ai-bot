179dac08d83461ad1f1b7e3f80bf96ff9bd656c6
lucas <santanafx@hotmail.com>
2025-03-17 20:10:50 -0300

    feat: Remove Chat, Header, OutputArea, and Text components along with their styles and interfaces

--- src/components/atoms/Chat/Chat.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface ChatProps {
-  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
-  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  isLoadingChat: boolean;
-  input: string;
-}
--- src/components/atoms/Text/Text.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface TextProps {
-  children: React.ReactNode;
-  type?: "h1" | "h2" | "h3" | "h4" | "p" | "span";
-  color?: string;
-}
