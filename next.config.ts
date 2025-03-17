2d905dbf5ee19e215625567bb8e08d4e600640ef
lucas <santanafx@hotmail.com>
2025-03-16 21:56:14 -0300

    feat: Configure local image patterns in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    localPatterns: [
+      {
+        pathname: "/assets/images/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
